e73e20eb3bf724c35e2a5abc64d0ce9c044823b3
darshangoswami <goswamidarshan999@gmail.com>
2024-03-02 14:41:36 -0500

    multi-player support added

--- PIG.py
+++ PIG.py
@@ -1,9 +1,8 @@
 import random
 
 def roll():
-  value = random.randint(1,6)
-
-  return value
+  roll = random.randint(1,6)
+  return roll
 
 while True:
   players = input("Do we have 2 players,3 players or 4 players here?")
@@ -18,24 +17,28 @@ while True:
     print("Please enter a valid number.")
 
 max_score = 50
-
 players_scores = [0 for i in range(players)]
 
 while max(players_scores) < max_score:
+  for player_id in range(players):
+    print("\nPlayer number", player_id + 1, "'s turn to roll.\n")
+    current_score = 0
 
-  current_score = 0
+    while True:
+      want_roll = input("Would you like to roll (y)")
+      if want_roll.lower() != "y":
+        break
+      value = roll()
 
-  want_roll = input("Would you like to roll (y)")
+      if value == 1:
+        print("You rolled a 1, you're done!")
+        current_score = 0
+        break
+      else:
+        current_score += value
+        print("You rolled a :", value)
 
-  if want_roll.lower != "y":
-    break
-  value = roll()
+      print("Your current score is:", current_score)
 
-  if value == 1:
-    print("You rolled a 1, you're done!")
-    break
-  else:
-    current_score += value
-    print("You rolled a :", value)
-
-  print("Your current score is:", current_score)
+    players_scores[player_id] += current_score
+    print("Your total score is:", players_scores[player_id])
